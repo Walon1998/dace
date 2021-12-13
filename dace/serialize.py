661db863c49d0508cbe8e5f9d5afba2fc6e2ab96
Neville Walo <neville.walo@gmail.com>
2021-12-13 10:39:38 +0100

    remove simplify code

--- dace/serialize.py
+++ dace/serialize.py
@@ -226,10 +226,7 @@ def set_properties_from_json(object_with_properties,
 
         setattr(object_with_properties, prop_name, val)
 
-        #Simplify volume to pass serialization tests with vectorization transformation
-        # if prop_name == 'volume':
-        #     object_with_properties.volume = \
-        #          object_with_properties.volume.simplify()
+
 
     remaining_properties = source_properties - ignore_properties
     # Ignore all metadata "properties" saved for DIODE
